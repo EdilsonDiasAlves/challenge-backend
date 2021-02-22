0cf31b558515173de563b1ccacdef9997c4c9056
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-02-21 21:41:44 -0300

    Adding query params and implementing filters

--- src/main/java/com/mz/bitchallenge/controllers/ItemController.java
+++ src/main/java/com/mz/bitchallenge/controllers/ItemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mz.bitchallenge.dtos.ItemDTO;
@@ -16,9 +17,13 @@ public class ItemController {
 	@Autowired
 	private ItemService itemService;
 	
-	@GetMapping(value = "/items", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<ItemDTO> getItem() {
-		List<ItemDTO> itemsReturned = itemService.getItems();
+	@GetMapping(value = "/items", 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	
+	public List<ItemDTO> getItem(
+			@RequestParam(name = "begindate", required = false) String beginDate,
+			@RequestParam(name = "finaldate", required = false) String finalDate) {
+		List<ItemDTO> itemsReturned = itemService.getItems(beginDate, finalDate);
 		return itemsReturned;
 	}
 }
--- src/main/java/com/mz/bitchallenge/services/ItemService.java
+++ src/main/java/com/mz/bitchallenge/services/ItemService.java
@@ -1,11 +1,18 @@
 package com.mz.bitchallenge.services;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import com.mz.bitchallenge.dtos.ItemDTO;
@@ -19,14 +26,39 @@ public class ItemService {
 	@Value("${items.address}")
 	private String itemAddress;
 	
-	public List<ItemDTO> getItems() {
+	public List<ItemDTO> getItems(String beginDate, String finalDate) {
 		
 		ItemDTO[] responseItems = restTemplate.getForObject(itemAddress, ItemDTO[].class);
 		List<ItemDTO> items = Arrays.asList(responseItems);
 		
-		// To convert and filter after
-		// Date date = Date.from(Instant.parse("2016-10-01T14:30:37.040Z"));
+		if(StringUtils.hasText(beginDate) && StringUtils.hasText(finalDate)) {
+			
+			try {
+				return performFilter(beginDate, finalDate, items);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
 		
 		return items;
+	}
+
+	private List<ItemDTO> performFilter(String beginDate, String finalDate, List<ItemDTO> items) throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		Date beginDateParsed = formatter.parse(beginDate);
+		Date finalDateParsed = formatter.parse(finalDate);
+		
+		// Adding one day to final Date (the final date til 23:59:59 must be considered on the result filter)
+		LocalDateTime finalDatePlusADay = LocalDateTime.from(finalDateParsed.toInstant().atZone(ZoneId.of("UTC"))).plusDays(1);
+		finalDateParsed = Date.from(finalDatePlusADay.atZone(ZoneId.systemDefault()).toInstant());
+		
+		List<ItemDTO> filteredItems = new ArrayList<>();
+		
+		for (ItemDTO itemDTO : items) {
+			if(itemDTO.getDate().after(beginDateParsed) && itemDTO.getDate().before(finalDateParsed)) {
+				filteredItems.add(itemDTO);
+			}
+		}
+		return filteredItems;
 	}
 }
